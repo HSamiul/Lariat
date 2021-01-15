169f09577ae2634ea671924acf2dfaa35a6466e6
Samiul Hoque <39920503+HSamiul@users.noreply.github.com>
2021-01-15 11:15:09 -0500

    Updated key textfield

--- LoginView.swift
+++ LoginView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LoginView.swift
 //  LariatUI
 //
 //  Created by Samiul Hoque on 1/14/21.
@@ -16,9 +16,7 @@ extension NSTextField {
         set { }
     }
     
-    //open override func removeCursorRect(_ rect: NSRect, cursor object: NSCursor) {
-        
-    //}
+
 }
 
 //  NSViewRepresentable is a protocol that this custom View I'm making conforms to
@@ -30,18 +28,16 @@ struct OpaqueWindow: NSViewRepresentable {
         view.material = .sidebar
         return view
     }
-
     func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
     }
 }
 
-
-
 //  View is a protocol. My struct (Login) must conform to the variables and methods defined in its protocol. E.g., body is a variable somewhere in View.
 struct ContentView: View {
     var screen = NSScreen.main?.visibleFrame
-    @State private var text = ""
+    let lightGreyColor = Color(red: 255, green: 255, blue: 255, opacity: 0.05)
 
+    @State private var login = ""
     //  some View is a specific structure that is compatible with the contents of the body. I don't know what that structure is, but the compiler will.
     
     //  Furthermore, below I'm declaring the type of the body, not setting it equal to anything. That type will be determined by what I've commented above.
@@ -50,24 +46,26 @@ struct ContentView: View {
         VStack {
             Image("logo")
                 .resizable()
-                .frame(width: 256, height: 135)
-            
+                .frame(width: 250, height: 135)
             HStack {
                 Image(systemName: "person.crop.circle")
                     .resizable()
                     .frame(width: 15, height: 15)
                     .foregroundColor(.gray)
                 
-                SecureField("Enter your key", text: $text)
+                SecureField("•••••••••••••••••••••••••", text: $login)
                     .frame(width: 150, height: nil)
-                    .controlSize(.large)
-                    .textFieldStyle(PlainTextFieldStyle())
+                    .multilineTextAlignment(.leading)
+                    .textFieldStyle(PlainTextFieldStyle()) // love you
                     .font(Font.system(size: 15, weight: .light, design: .default))
-                    .multilineTextAlignment(TextAlignment.center)
-                    .foregroundColor(.gray)
+                
             }
+            .frame(width: 300, height: 50)
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
         }
         .frame(width: screen!.width / 3, height: screen!.height / 3)
+        .offset(y:-25)
         .background(OpaqueWindow())
     }
 }
@@ -77,5 +75,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
 
